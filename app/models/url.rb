7540ffb9fcb0c4fb55250a7cfc60497159eebe11
Alex <96dalex@gmail.com>
2015-02-09 09:35:30 -0700

    Update url.rb

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,9 +2,10 @@ class Url < ActiveRecord::Base
   scope :popular, -> { order(popularity: :desc) }
   scope :newest, -> { order(:created_at) }
 
-  before_save { |url| url.short = url.generate_short_url }
+  before_save :generate_short_url
 
   def generate_short_url
-    ('a'..'z').to_a.shuffle[0,5].join
+    url.short = ('a'..'z').to_a.shuffle[0,5].join
+    true
   end
 end
